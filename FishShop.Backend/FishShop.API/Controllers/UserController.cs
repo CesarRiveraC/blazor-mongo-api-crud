beaf0d2a1e4d65d35b1457356a30978436ab9879
Cesar Rivera Campos <cesarrivcam@gmail.com>
2021-05-30 01:20:16 -0600

    New structure Front, UI added

---
+++ FishShop.Backend/FishShop.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishShop.API.Controllers
+{
+    public class UserController
+    {
+    }
+}
---
+++ FishShop.Backend/FishShop.Core/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishShop.Core.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string BasePrice { get; set; }
+
+        public string SalePrice { get; set; }
+
+    }
+}
